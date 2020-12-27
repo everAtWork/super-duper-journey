309acd6f3139efa0c7ff4f7cd2c6baaef115b6e8
Wladyslaw Ko <ever.at.work@gmail.com>
2020-12-27 22:07:24 +0200

    только стили допилить

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { SortingTable } from './components/SortingTable';
-
+import MOCK_DATA from './components/MOCK_DATA.json'
 
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
   return ( 
   <div className="App">
+    <input type="text" placeholder="Search..." onChange={event => {
+      setSearchTerm(event.target.value)
+    }} />
+    {MOCK_DATA.filter(val => {
+      if (searchTerm == ""){
+        return val
+      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+      }
+    }).map((val,key) => {
+      return <div className="user" key={key}>{val.name}</div>
+    })}
     <SortingTable></SortingTable>
   </div>
   )
