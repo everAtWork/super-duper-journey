b77d139e59727f51a09e56937f930390ed42b6ab
Wladyslaw Ko <ever.at.work@gmail.com>
2020-12-28 13:30:09 +0200

    before I redefine the search..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import { SortingTable } from './components/SortingTable';
-import MOCK_DATA from './components/MOCK_DATA.json'
+import MOCK_DATA from './MOCK_DATA.json'
 
 
 function App() {
@@ -12,13 +12,13 @@ function App() {
       setSearchTerm(event.target.value)
     }} />
     {MOCK_DATA.filter(val => {
-      if (searchTerm == ""){
+      if (searchTerm === ""){
         return val
       } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
         return val
       }
     }).map((val,key) => {
-      return <div className="user" key={key}>{val.name}</div>
+      return <div className="user">{`${key} ___` + val.name }</div>
     })}
     <SortingTable></SortingTable>
   </div>
